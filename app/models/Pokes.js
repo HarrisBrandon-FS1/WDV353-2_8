fb3848e40d14ba624aeec1d43d09147544bc0569
HarrisBrandon-FS1 <162524452+HarrisBrandon-FS1@users.noreply.github.com>
2025-03-18 15:50:19 -0600

    updated

---
+++ app/models/Pokes.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const pokeSchema = new mongoose.Schema({
+    name: {
+        type:String,
+        unique: true,
+        required: [true, "you are required to have a Pokemon"],
+        trim: true,
+        maxlength: [25, "That name is too long"],
+    },
+    type: {
+        type: [String],
+        required:true,
+        enum: [
+            "Normal",
+            "Fire",
+            "Fighting",
+            "Water",
+            "Flying",
+            "Grass",
+            "Poison",
+            "Electric",
+            "Ground",
+            "Psychic",
+            "Rock",
+            "Ice",
+            "Bug",
+            "Dragon",
+            "Ghost",
+            "Dark",
+            "Steel",
+            "Fairy", 
+        ],
+    },
+    dexNumber: {
+        type: Number,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: [true, "Please add a description"],
+        maxlength: [500, "Description cannot be over 500 characters"],
+    },
+},
+{timestamps: true}
+);
+
+module.exports = mongoose.model('Pokemon', pokeSchema);
